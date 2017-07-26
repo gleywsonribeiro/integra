c577711649a4f8bb7458ed36908257bd967b5db2
Gleywson <Gleywson@hmue00118605>
2017-07-26 10:00:27 -0300

    Adicionado mecanismo de exportação

--- src/main/java/br/com/hmue/integra/controller/OrdemServicoController.java
+++ src/main/java/br/com/hmue/integra/controller/OrdemServicoController.java
@@ -14,6 +14,12 @@ import java.util.Map;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.primefaces.context.RequestContext;
 
 /**
@@ -60,5 +66,21 @@ public class OrdemServicoController implements Serializable{
     
     public void fecharDialogo() {
         RequestContext.getCurrentInstance().closeDialog(null);
+    }
+    
+    public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow header = sheet.getRow(0);
+         
+        HSSFCellStyle cellStyle = wb.createCellStyle();  
+        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
+        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+         
+        for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
+            HSSFCell cell = header.getCell(i);
+             
+            cell.setCellStyle(cellStyle);
+        }
     }
 }
