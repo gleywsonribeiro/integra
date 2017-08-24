339633aa436c9c62e9beaafc322122686fd006a1
Gleywson <Gleywson@hmue00118605>
2017-08-24 11:19:40 -0300

    Tratamento de exceção em caso de falha de conexão com o banco de dados

--- src/main/java/br/com/hmue/integra/factory/ConnectionFactory.java
+++ src/main/java/br/com/hmue/integra/factory/ConnectionFactory.java
@@ -14,17 +14,25 @@ import java.util.logging.Logger;
 public class ConnectionFactory {
 
     private static final String USERNAME = "dbamv";
-    private static final String PASSWORD = "master123";
+    private static final String PASSWORD = "prosaude123";
     private static final String SERVER = "10.79.100.15";
     private static final String PORT = "1521";
     private static final String DATABASE = "prd";
     //Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
     private static final String DATABASE_URL = "jdbc:oracle:thin:@" + SERVER + ":" + PORT + ":" + DATABASE;
 
-    public static Connection createConnectionToOracle() throws ClassNotFoundException, SQLException {
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-
-        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+    public static Connection createConnectionToOracle() {
+        Connection connection = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            
+            connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         return connection;
     }
 
